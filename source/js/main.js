05c2c4efec15dee2047b78d56760d84ecb5ac676
wangriyu <wangriyu1997@gmail.com>
2017-09-29 19:23:10 +0800

    添加404文字效果并修改字数统计的样式

--- source/js/main.js
+++ source/js/main.js
@@ -26,7 +26,7 @@ require([], function (){
             mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
             ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
             android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPhone: u.indexOf('iPhone') > -1, // || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
             iPad: u.indexOf('iPad') > -1, //是否为iPad
             webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
             weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
@@ -183,9 +183,8 @@ require([], function (){
 
         var $current = $(this);
         function update(str, check) {
-            var click = ["disabled", ""];
             $current.html($current.html().replace(
-              str, "<input type='checkbox' " + check + " " + click[1] + " >")
+              str, "<input type='checkbox' " + check + " disabled >")
             )
         }
 
@@ -198,6 +197,5 @@ require([], function (){
                 update(string[0], "");
             }
         }
-    })
-
+    });
 })
